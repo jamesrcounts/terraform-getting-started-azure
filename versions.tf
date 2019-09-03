92efa0ab3361d27383cfe1d73d2aafe473d7704e
James Counts <jamesrcounts@outlook.com>
2019-09-03 07:17:50 -0700

    Remote state
    Add backend script
    Configure terraform

--- versions.tf
+++ versions.tf
@@ -1,4 +1,9 @@
-
 terraform {
   required_version = ">= 0.12"
+  backend "azurerm" {
+    resource_group_name  = "terraformrg"
+    storage_account_name = "terraformde3fb0d9c9a7413"
+    container_name       = "terraform"
+    key                  = "terraform-getting-started.tfstate"
+  }
 }
