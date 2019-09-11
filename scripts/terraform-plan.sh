4db37648dedd0ab7c0663c8397b5abd9d5a8a535
James Counts <jamesrcounts@outlook.com>
2019-09-11 06:55:50 -0700

    Debug Plan

--- scripts/terraform-plan.sh
+++ scripts/terraform-plan.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+WORKING_DIRECTORY=${1}
+cd ${WORKING_DIRECTORY}
+
+export ARM_CLIENT_ID=${servicePrincipalId}
+export ARM_CLIENT_SECRET=${servicePrincipalKey}
+export ARM_SUBSCRIPTION_ID=$(az account show --query "id" -o tsv)
+export ARM_TENANT_ID=${tenantId}
+
 terraform plan \
   -input=false \
   -no-color \
-  -out $(Build.BuildNumber).tfplan
+  -out ${BUILD_BUILDNUMBER}.tfplan
