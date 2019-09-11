f499607c75bd8658eab71198d84a6e3c553c2676
James Counts <jamesrcounts@outlook.com>
2019-09-11 06:28:17 -0700

    Debugged Init

--- scripts/terraform-init.sh
+++ scripts/terraform-init.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+WORKING_DIRECTORY=${1}
+cd ${WORKING_DIRECTORY}
+
+SUBSCRIPTION_ID=$(az account show --query "id" -o tsv)
+
 terraform init \
   -input=false \
   -no-color \
-  --backend-config="client_id=${AZURE_CLIENT_ID}" \
-  --backend-config="client_secret=${AZURE_CLIENT_SECRET}" \
-  --backend-config="subscription_id=${AZURE_SUBSCRIPTION_ID}" \
-  --backend-config="tenant_id=${AZURE_TENANT_ID}"
+  --backend-config="client_id=${servicePrincipalId}" \
+  --backend-config="client_secret=${servicePrincipalKey}" \
+  --backend-config="subscription_id=${SUBSCRIPTION_ID}" \
+  --backend-config="tenant_id=${tenantId}"
