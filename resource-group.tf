dcdf12d2c4b8caa9b095722f1a2422936250410a
Jim Counts <jamesrcounts@outlook.com>
2021-05-06 13:33:54 +0000

    Remove old resource group

--- resource-group.tf
+++ resource-group.tf
@@ -1,13 +1,4 @@
-# Create a new resource group
-resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "westus2"
-
-  tags = {
-    environment = "demo"
-  }
-}
-
+# Read details about the resource group created for this project.
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
