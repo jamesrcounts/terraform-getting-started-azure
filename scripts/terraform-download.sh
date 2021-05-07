8752130d1741f120e6e220398e66f3c3269cb298
Jim Counts <jamesrcounts@outlook.com>
2021-05-07 15:23:48 +0000

    Move pin-terraform to template

--- scripts/terraform-download.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-curl -SL "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" --output terraform.zip
-echo "${TERRAFORM_DOWNLOAD_SHA} terraform.zip" | sha256sum -c -
-unzip "terraform.zip"
-sudo mv terraform /usr/local/bin
-terraform --version
-rm terraform.zip
