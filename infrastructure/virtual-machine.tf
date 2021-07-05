fd778e7a12a47e775d6459f792e2f8b4f2fbe237
Jim Counts <jamesrcounts@outlook.com>
2021-07-05 19:33:37 +0000

    Update disk size for demo

--- infrastructure/virtual-machine.tf
+++ infrastructure/virtual-machine.tf
@@ -19,7 +19,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   os_disk {
     caching              = "ReadWrite"
-    disk_size_gb         = 1024
+    disk_size_gb         = 32
     storage_account_type = "Premium_LRS"
     name                 = "vm-${local.instance_id}-os"
   }
