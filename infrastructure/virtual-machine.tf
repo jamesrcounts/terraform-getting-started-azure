035dec332bc66bc137d4915867afc98c31f7d3ef
Jim Counts <jamesrcounts@outlook.com>
2021-07-05 19:27:30 +0000

    Update password requirements

--- infrastructure/virtual-machine.tf
+++ infrastructure/virtual-machine.tf
@@ -34,6 +34,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
 resource "random_password" "admin_password" {
   length           = 16
+  min_lower        = 1
+  min_numeric      = 1
+  min_special      = 1
+  min_upper        = 1
   override_special = "_%@"
   special          = true
 }
