fd27357421da78a9f6621152fb7079f9f71609da
Jim Counts <jamesrcounts@outlook.com>
2021-11-27 16:04:13 +0000

    Add environment subscription id.

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,3 +2,8 @@ variable "resource_group_name" {
   description = "(Required) The name of the resource group for this project."
   type        = string
 }
+
+variable "env_subscription_id" {
+  description = "(Required) The Azure subscription ID for the solution environment."
+  type        = string
+}
